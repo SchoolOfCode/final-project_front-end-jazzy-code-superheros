ceb0d13b5aad53ecbf6110ca6119acbd237c7d1b
J05hL <josh.langford@hotmail.co.uk>
2022-07-27 11:13:29 +0100

    added fetch request and useEffect to call on first render and useState to store the payload

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -1,8 +1,38 @@
+import {useState, useEffect} from 'react'
+
+
 import AboutUs from "../About";
 import Button from "../Button";
 import "./style.css"
 
+
+
 const Home = () => {
+
+  const [foodBankData, setFoodBankData] = useState()
+
+  const foodbankName = 'vauxhall'
+
+  let url = `https://www.givefood.org.uk/api/2/foodbank/`
+
+  const getFoodBank = async ()=> {
+
+  const response = await fetch(`${url}${foodbankName}/`)
+
+  const data = await response.json();
+  console.log(`data in func >>>`, data)
+
+  setFoodBankData(data)
+
+  }
+
+  useEffect(()=> {
+    getFoodBank()
+
+}, [])
+
+  console.log(`foodBankData state >>>`, foodBankData)
+
   return(
     <div className="Home">
       <div className="ButtonContainer">
