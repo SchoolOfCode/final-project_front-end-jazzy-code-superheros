9bf1185016c0b54be0d8a79ced46d9c1494de2da
Camilla Santos <milla.revolts@gmail.com>
2022-07-27 15:51:38 +0100

    changed one of the images for the card

--- src/Components/Carousel/CarouselData.js
+++ src/Components/Carousel/CarouselData.js
@@ -10,9 +10,9 @@ export const CarouselData = [
         testimony: "Life is great when we work together as a community",
          urlImg:"https://www.allprodad.com/wp-content/uploads/2021/03/05-12-21-happy-people.jpg"
     },  {
-        name: "Carlos",
+        name: "Maria",
         job: "Volunteer",
         testimony: "Life is great when we work together as a community",
-        urlImg:"https://wp.en.aleteia.org/wp-content/uploads/sites/2/2018/09/web3-happy-people-outside-smile-sun-nature-eduardo-dutra-620857-unsplash.jpg?w=620&h=348&crop=1"
+        urlImg:"https://media.istockphoto.com/photos/happy-africanamerican-woman-making-heart-with-hands-picture-id1306355552?b=1&k=20&m=1306355552&s=170667a&w=0&h=LCJQQwyCURqT-Ja8vQF4G81lDUifcHv9Yxv6o_E_rkQ="
     }
  ]
