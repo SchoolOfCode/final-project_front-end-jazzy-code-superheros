b4ff535d39a68f0b259bf2f7e14f67260bec8c5a
Simran Toor <simrankaurtoor@outlook.com>
2022-07-27 14:02:30 +0100

    Eligibility card basic structure

---
+++ src/Components/GetHelp/EligibilityCard/index.js
@@ -0,0 +1,29 @@
+function EligibilityCard() {
+    return (
+      <div className="container">
+        <h1>Eligibility Criteria</h1>
+        <p>
+          something about how food banks are there for anyone in the community who
+          needs them
+        </p>
+        <h2>Food Vouchers</h2>
+        <p>
+          In order to get help from the food bank you will need to be referred with a voucher
+        </p>
+  
+        <h3>Referral Process</h3>
+        <p>
+          Each food bank works with different frontline professionals, like:
+          <ul>
+              <li>doctors</li>
+              <li>health visitors</li>
+              <li>social workers</li>
+              <li>citizens advice</li>
+          </ul>
+        </p>
+        <h3>Using Your Voucher</h3>
+      </div>
+    );
+  }
+  
+  
