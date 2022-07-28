bdfc1602cc786cc83648243b14c21a8a0b3665dd
Camilla Santos <milla.revolts@gmail.com>
2022-07-28 11:49:59 +0100

    facts data created on the file factsdata

--- src/Components/Facts/factsdata.js
+++ src/Components/Facts/factsdata.js
@@ -1,16 +1,23 @@
+import hope from "../../img/hope.jpg"
+import values from "../../img/values.jpg"
+import vision from "../../img/vision.jpg"
+
 export const factsData=[
 {
+    img:{vision},
     fact:"Our Vision",
     explanation:"Our vision is to end the need for food banks."
 
 },
 
 {
+    img:{hope},
     fact:"Our Hope",
     explanation:"We believe it's not right that anyone cannot afford their own food. That's why we are working towards a just, compassionate future, where no one should have to use a food bank to get by."    
 
 },
 {   
+    img:{values},
     fact:"Our Values",
     explanation:"Our values of compassion, justice, community and dignity, underpin everything we do. It isn’t right that anyone should experience hunger or poverty, and we know we all have a responsibility to stand alongside people in crisis."
 
