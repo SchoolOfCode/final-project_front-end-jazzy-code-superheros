8a473e3eba90603caf1a6c6ed08355ac89cb686a
Camilla Santos <milla.revolts@gmail.com>
2022-08-05 15:55:28 +0100

    map working and done

--- src/Components/Map/Map.js
+++ src/Components/Map/Map.js
@@ -3,8 +3,8 @@ import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
 import "./Map.css";
 
 const containerStyle = {
-  width: '400px',
-  height: '400px'
+  width:"100%",
+  height: '50vh'
 };
 
 
