af00b934b6d57a903922fc3b051d75a353ff6392
Camilla Santos <milla.revolts@gmail.com>
2022-08-08 16:01:59 +0100

    form is almost there

--- src/Components/Cms/FormData/index.js
+++ src/Components/Cms/FormData/index.js
@@ -7,15 +7,29 @@ const FormData = () => {
 
   function handleAdd(e) {
     e.preventDefault();
-    console.log(needs);
   }
 
   function handleDelete(e) {
     e.preventDefault();
 
     setNeeds(needs.filter((item) => item.id !== Number(e.target.value)));
-    console.log(needs);
   }
+  // return (
+  //   <Formik
+  //      initialValues={{
+  //        'owner.fullname': '',
+  //      }}
+  //      onSubmit={values => {
+  //        // same shape as initial values
+  //        console.log(values);
+  //      }}
+  //    >
+  //      <Form>
+  //        <Field name="['owner.fullname']" />
+  //        <button type="submit">Submit</button>
+  //      </Form>
+  //    </Formik>
+  // )
 
   return (
     <Formik
@@ -44,9 +58,6 @@ const FormData = () => {
         if (!values.email || !emailRegex.test(values.email)) {
           errors.email = "Valid Email Required";
         }
-        if (!values.message) {
-          errors.message = "Message Required";
-        }
         return errors;
       }}
     >
@@ -59,142 +70,151 @@ const FormData = () => {
         handleSubmit,
         isSubmitting,
         /* and other goodies */
-      }) => (
-        <Form
-          name="needs-form"
-          data-netlify="true"
-          method="POST"
-          onSubmit={handleSubmit}
-        >
-          <div className="row">
+      }) => {
+        console.log(errors);
+        return (
+          <Form name="needs-form" data-netlify="true" method="POST">
+            <div className="row">
+              <div className="col-25">
+                <label htmlFor="name">Name: </label>
+              </div>
+              <div className="col-75">
+                <Field className="inputs" name="name" placeholder="Name" />
+                <p className="error-message">
+                  <ErrorMessage name="name" />
+                </p>
+              </div>
+            </div>
             <div className="col-25">
-              <label htmlFor="name">Name: </label>
+              <label htmlFor="address">Address: </label>
             </div>
             <div className="col-75">
-              <Field className="inputs" name="name" placeholder="Name" />
+              <Field className="inputs" name="address" placeholder="Address" />
               <p className="error-message">
-                <ErrorMessage name="name" />
+                <ErrorMessage name="address" />
               </p>
             </div>
-          </div>
-          <div className="col-25">
-            <label htmlFor="address">Address: </label>
-          </div>
-          <div className="col-75">
-            <Field className="inputs" name="address" placeholder="Address" />
-            <p className="error-message">
-              <ErrorMessage name="address" />
-            </p>
-          </div>
-          <div className="col-25">
-            <label htmlFor="postcode">Post-code </label>
-          </div>
-          <div className="col-75">
-            <Field className="inputs" name="postcode" placeholder="Post-Code" />
-            <p className="error-message">
-              <ErrorMessage name="postcode" />
-            </p>
-          </div>
-          <div className="row">
             <div className="col-25">
-              <label htmlFor="phone">Phone </label>
-            </div>
-            <div className="col-75">
-              <Field className="inputs" name="phone" placeholder="Phone" />
-              <p className="error-message">
-                <ErrorMessage name="phone" />
-              </p>
-            </div>
-          </div>
-          <div className="row">
-            <div className="col-25">
-              <label htmlFor="email">Email</label>
-            </div>
-            <div className="col-75">
-              <Field className="inputs" name="email" placeholder="Email" />
-              <p className="error-message">
-                <ErrorMessage name="email" />
-              </p>
-            </div>
-          </div>
-          <div className="row">
-            <div className="col-25">
-              <label htmlFor="imgurl">Image Url </label>
-            </div>
-            <div className="col-75">
-              <Field className="inputs" name="imgurl" placeholder="Image Url" />
-              <p className="error-message">
-                <ErrorMessage name="imgurl" />
-              </p>
-            </div>
-          </div>
-          <div className="row">
-            <div className="col-25">
-              <label htmlFor="needs">Needs </label>
+              <label htmlFor="postcode">Post-code </label>
             </div>
             <div className="col-75">
               <Field
                 className="inputs"
-                name="needs"
-                placeholder="Needs"
-                onChange={handleChange}
+                name="postcode"
+                placeholder="Post-Code"
               />
-              {console.log(values)}
+              <p className="error-message">
+                <ErrorMessage name="postcode" />
+              </p>
+            </div>
+            <div className="row">
+              <div className="col-25">
+                <label htmlFor="phone">Phone </label>
+              </div>
+              <div className="col-75">
+                <Field className="inputs" name="phone" placeholder="Phone" />
+                <p className="error-message">
+                  <ErrorMessage name="phone" />
+                </p>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-25">
+                <label htmlFor="email">Email</label>
+              </div>
+              <div className="col-75">
+                <Field className="inputs" name="email" placeholder="Email" />
+                <p className="error-message">
+                  <ErrorMessage name="email" />
+                </p>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-25">
+                <label htmlFor="imgurl">Image Url </label>
+              </div>
+              <div className="col-75">
+                <Field
+                  className="inputs"
+                  name="imgurl"
+                  placeholder="Image Url"
+                />
+                <p className="error-message">
+                  <ErrorMessage name="imgurl" />
+                </p>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-25">
+                <label htmlFor="needs">Needs </label>
+              </div>
+              <div className="col-75">
+                <Field
+                  className="inputs"
+                  name="needs"
+                  placeholder="Needs"
+                  onChange={handleChange}
+                />
 
-              <button onClick={handleAdd}>+</button>
-              <p className="error-message">
-                <ErrorMessage name="needs" />
-              </p>
+                <button onClick={handleAdd}>+</button>
+                <p className="error-message">
+                  <ErrorMessage name="needs" />
+                </p>
+              </div>
             </div>
-          </div>
-          {needs.map((needsData) => {
-            return (
-              <p>
-                {needsData.name}
-                <button value={needsData.id} onClick={handleDelete}>
-                  -
-                </button>
-              </p>
-            );
-          })}
+            {needs.map((needsData) => {
+              return (
+                <p>
+                  {needsData.name}
+                  <button value={needsData.id} onClick={handleDelete}>
+                    -
+                  </button>
+                </p>
+              );
+            })}
 
-          <div className="row">
-            <div className="col-25">
-              <label htmlFor="distance">Distance </label>
+            <div className="row">
+              <div className="col-25">
+                <label htmlFor="distance">Distance </label>
+              </div>
+              <div className="col-75">
+                <Field
+                  className="inputs"
+                  name="distance"
+                  placeholder="Distance"
+                />
+                <p className="error-message">
+                  <ErrorMessage name="distance" />
+                </p>
+              </div>
             </div>
-            <div className="col-75">
-              <Field
-                className="inputs"
-                name="distance"
-                placeholder="Distance"
-              />
-              <p className="error-message">
-                <ErrorMessage name="distance" />
-              </p>
+            <div className="row">
+              <div className="col-25">
+                <label htmlFor="lat_lng">Latitude and Longitude</label>
+              </div>
+              <div className="col-75">
+                <Field
+                  className="inputs"
+                  name="lat_lng"
+                  placeholder="Latitude and Longitude"
+                />
+                <p className="error-message">
+                  <ErrorMessage name="lat_lng" />
+                </p>
+              </div>
             </div>
-          </div>
-          <div className="row">
-            <div className="col-25">
-              <label htmlFor="lat_lng">Latitude and Longitude</label>
+            <div className="col-100">
+              <button
+                type="submit"
+                disabled={isSubmitting}
+                className="submit-admin"
+              >
+                Send
+              </button>
             </div>
-            <div className="col-75">
-              <Field
-                className="inputs"
-                name="lat_lng"
-                placeholder="Latitude and Longitude"
-              />
-              <p className="error-message">
-                <ErrorMessage name="lat_lng" />
-              </p>
-            </div>
-          </div>
-          <div className="col-100">
-            <button type="submit" className="submit-admin">
-              Send
-            </button>
-          </div>
-        </Form>
-      )}
+          </Form>
+        );
+      }}
     </Formik>
   );
 };
