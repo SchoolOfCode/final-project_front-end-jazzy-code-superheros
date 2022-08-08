d6fe6fc36b7efb8c549d6544a18a42da1518b788
Simran Toor <simrankaurtoor@outlook.com>
2022-08-08 11:42:31 +0100

    imported useState, useCallback, memo from react and deleted REACT from REACT.useState etc

--- src/Components/Map/Map.js
+++ src/Components/Map/Map.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import { useState, useCallback, memo} from 'react'
 import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
 import "./Map.css";
 
@@ -6,7 +6,6 @@ const containerStyle = {
   width:"100%",
   height: '50vh'
 };
-
 
 const MyComponent = ({ foodBankData }) => {
 
@@ -24,16 +23,16 @@ const MyComponent = ({ foodBankData }) => {
     lng: Number(latLng[1])
   }
 
-  const [map, setMap] = React.useState(null)
+  const [map, setMap] = useState(null)
 
-  const onLoad = React.useCallback(function callback(map) {
+  const onLoad = useCallback(function callback(map) {
     const bounds = new window.google.maps.LatLngBounds(center);
     map.fitBounds(bounds);
     setMap(map)
   }, [])
   console.log(map)
 
-  const onUnmount = React.useCallback(function callback(map) {
+  const onUnmount = useCallback(function callback(map) {
     setMap(null)
   }, [])
 
@@ -52,4 +51,4 @@ const MyComponent = ({ foodBankData }) => {
       </div>
   ) : <div ClassName='map'></div>
 }
-export default React.memo(MyComponent)
+export default memo(MyComponent)
