ab287cbfa7a5b2ec4f07b23129c2ecba22f08a2d
skandog <muumrar@gmail.com>
2022-08-09 15:54:30 +0100

    created framer motion contaner to add delay entrance transition

--- src/Components/GiveHelp/index.js
+++ src/Components/GiveHelp/index.js
@@ -4,7 +4,22 @@ import Button from "../Button";
 import Container from "../Map/Map.js";
 import "./style.css";
 import D from "../StyledDiv";
-import { motion } from 'framer-motion';
+import { motion } from "framer-motion";
+
+const StyledDiv = motion.div;
+
+const StyleD = ({ children, delay = 0 }) => {
+  return (
+    <StyledDiv
+      initial={{ y: 10, opacity: 0 }}
+      animate={{ y: 0, opacity: 1 }}
+      transition={{ duration: 0.8, delay }}
+      mb={6}
+    >
+      {children}
+    </StyledDiv>
+  );
+};
 
 const GiveHelp = ({ foodBankData }) => {
   return (
@@ -18,45 +33,50 @@ const GiveHelp = ({ foodBankData }) => {
           alt="container cover, people giving help"
         ></img>
       </div>
-
-      <ToggleCardGeneral
-        cardTitle="Donate Money"
-        cardBody="Foodbanks are mostly volunteer run organisations, 
+      <StyleD delay={0.1}>
+        <ToggleCardGeneral
+          cardTitle="Donate Money"
+          cardBody="Foodbanks are mostly volunteer run organisations, 
         and rely on contributions from the local community. 
         While many people think of giving food first and foremost, 
         there is also an urgent need for cash donations; 
         this helps with funding further services and community groups to help those in need.
         "
-      >
-        <a href={foodBankData.urls.homepage} target="_blank" rel="noreferrer">
-          <Button nameButton="Donate" />
-        </a>
-      </ToggleCardGeneral>
-
-      <ToggleCardGeneral cardTitle="Donate Items">
-        <D>
-          <p>
-            Your local foodbank will be grateful for whatever you can share.
-            However, there are usually items that are more in need.
-          </p>
-          <p style={{ "font-weight": "bold" }}>
-            If {foodBankData.name} has any special requests we have listed them
-            below:
-          </p>
-        </D>
-        <List foodBankData={foodBankData} />
-      </ToggleCardGeneral>
-
-      <ToggleCardGeneral
-        cardTitle="Donate Time"
-        cardBody={`Any help will be appreciated, but especially valuable is any time you can share to help run our foodbank. 
-        Even if just a few hours a week, we would welcome your application. Please see ${foodBankData.name}'s website for specific details on how to volunteer`}
-      >
-        <a href={foodBankData.urls.homepage} target="_blank" rel="noreferrer">
-          <Button nameButton="Volunteer" />
-        </a>
+        >
+          <a href={foodBankData.urls.homepage} target="_blank" rel="noreferrer">
+            <Button nameButton="Donate" />
+          </a>
         </ToggleCardGeneral>
-      <Container foodBankData={foodBankData} />
+      </StyleD>
+      <StyleD delay={0.2}>
+        <ToggleCardGeneral cardTitle="Donate Items">
+          <D>
+            <p>
+              Your local foodbank will be grateful for whatever you can share.
+              However, there are usually items that are more in need.
+            </p>
+            <p style={{ "font-weight": "bold" }}>
+              If {foodBankData.name} has any special requests we have listed
+              them below:
+            </p>
+          </D>
+          <List foodBankData={foodBankData} />
+        </ToggleCardGeneral>
+      </StyleD>
+      <StyleD delay={0.3}>
+        <ToggleCardGeneral
+          cardTitle="Donate Time"
+          cardBody={`Any help will be appreciated, but especially valuable is any time you can share to help run our foodbank. 
+        Even if just a few hours a week, we would welcome your application. Please see ${foodBankData.name}'s website for specific details on how to volunteer`}
+        >
+          <a href={foodBankData.urls.homepage} target="_blank" rel="noreferrer">
+            <Button nameButton="Volunteer" />
+          </a>
+        </ToggleCardGeneral>
+      </StyleD>
+      <StyleD delay={0.4}>
+        <Container foodBankData={foodBankData} />
+      </StyleD>
     </div>
   );
 };
