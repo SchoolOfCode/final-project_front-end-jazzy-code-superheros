5a9ee155b944d32361247602010e5a996d886a21
Simran Toor <simrankaurtoor@outlook.com>
2022-08-10 10:38:23 +0100

    completely new landing-page carried over from forked version

--- src/Components/LandingPage/index.js
+++ src/Components/LandingPage/index.js
@@ -1,8 +1,7 @@
 import "./styles.css";
 import Input from "../Input/Input";
-import foodbanklogo from "../../img/ylfb1.png";
-
-
+import eggsBGMobile from "../../img/eggsBGMobile.jpg";
+import logo from "../../img/logo.png";
 
 const LandingPage = ({
   handleChange,
@@ -15,37 +14,52 @@ const LandingPage = ({
   console.log("Landing page rerenders");
   console.log(searchArray);
   return (
-    <div className="landing-container">
-      <img src={foodbanklogo} className="img-logo" alt="logo"></img>
-      <h3 className="styledh3">A lot of small foodbanks don't have the ways to pay for a website. Rather you want to give help or get help, Everything starts by typing your post code and selecting your foodbank</h3>
-      <Input
-        handleClick={handleClick}
-        handleChange={handleChange}
-        handleEnter={handleEnter}
-       className="input-landing"/>
-       <div className="admincontainer">
-       <h4 className="margin0">Are you an Admin or a FoodBank Manager ?</h4>
-       <button className="loginbutton"> Login <br></br>Sign up Now </button>
-       </div>
-    
-      {searchArray ? <h2 className="margin0">Please select a Food Bank: </h2> : null}
-      {array.map((item, index) => {
-        return (
-          <div className="search-results-container">
-            <div
-              className="search-results"
-              key={index}
-              onClick={() => setFoodBankData(searchArray[index])}
-            >
-              <h3 className="select-bank">{item.name}</h3>
-              <p>{item.address}</p>
-              <p>Distance from you {item.distance_mi.toFixed(1)} miles</p>
-            </div>
-          </div>
-        );
-      })}
-    </div>
+    <section>
+      <div className="intro">
+        <img src={eggsBGMobile} className="background-img" alt="" />
+        <img src={logo} className="logo" alt="logo" />
+        <div className="text">
+          <h1>We're here to help connect people to their local foodbanks</h1>
+          <p>
+            To see what's available in your area search using your postcode or
+            town below
+          </p>
+          <Input
+            handleClick={handleClick}
+            handleChange={handleChange}
+            handleEnter={handleEnter}
+            className="input-landing"
+          />
+          <h2>
+            For general information or if you're a charitable organisation
+            looking to use our free service to connect with your local community
+            (click here button)
+          </h2>
+        </div>
+      </div>
+      <div className="foodbank-list">
+        {searchArray ? (
+          <h2 className="margin0">Please select a Food Bank: </h2>
+        ) : null}
+        {array.map((item, index) => {
+          return (
+            <section className="search-results-container">
+              <div
+                className="search-results"
+                key={index}
+                onClick={() => setFoodBankData(searchArray[index])}
+              >
+                <h3 className="select-bank">{item.name}</h3>
+                <p>{item.address}</p>
+                <p>Distance from you {item.distance_mi.toFixed(1)} miles</p>
+              </div>
+            </section>
+          );
+        })}
+      </div>
+    </section>
   );
 };
 
 export default LandingPage;
+
