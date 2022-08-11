b5f0ad777e890d19a30408f33d26cc8e2cfc4b23
Camilla Santos <milla.revolts@gmail.com>
2022-08-11 11:04:38 +0100

    testing if the carousel renders on  the page and if its working

--- cypress/e2e/Foodbank.cy.js
+++ cypress/e2e/Foodbank.cy.js
@@ -34,4 +34,26 @@ describe('visiting the website', () => {
         cy.get('.ButtonContainer').contains('Give Help')
       })
   })
+
+  
+
+
+
+
+
+
 });
+
+it('testing the carousel on the home page', () => {
+  cy.visit('https://your-local-foodbank.netlify.app/')
+  cy.get('#inputbox').click('center').type('nw5 2dp')
+  cy.get(".button-input").click('center').then(() => {
+    cy.contains('Please select a Food Bank:')
+    cy.get('.search-results-container').should('not.have.text', 'Lewisham')
+    cy.get('.search-results-container').should('have.length', 5)
+    cy.get('.search-results-container').contains('Camden').click().then(() =>{
+      cy.get('.ButtonContainer').contains('Give Help')
+      cy.get('.right-arrow').click().then(() =>{cy.get('.Carousel').should('have.text', `JoshSupporter" We are on the search for new volunteers to join our team, check out the ways you can get involved "`)})
+    })
+})
+})
